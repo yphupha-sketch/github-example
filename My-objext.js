d1fcf7b5d9fdba50e9e1516b4c87d14799a5ca43
phupha <y.phupha@gmail.com>
2026-07-03 13:46:26 +0700

    add execldraw

--- My-objext.js
+++ My-objext.js
@@ -1,5 +1,5 @@
 let gamedatabase = [
-    { name: "limbus", genre: ["Action", "gacha"], image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1973530/header.jpg" }, 
-    { name: "elden ring", genre: ["RPG", "Adventure"], image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1245620/header.jpg" }, 
-    { name: "shadowverse", genre: ["cardgame"], image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/453480/header.jpg" }
+    { name: "limbus", genre: ["Action", "gacha"], image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1973530/header.jpg", url: "https://store.steampowered.com/app/1973530" },
+    { name: "elden ring", genre: ["RPG", "Adventure"], image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1245620/header.jpg", url: "https://store.steampowered.com/app/1245620" },
+    { name: "shadowverse", genre: ["cardgame"], image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/453480/header.jpg", url: "https://store.steampowered.com/app/453480" }
 ];
